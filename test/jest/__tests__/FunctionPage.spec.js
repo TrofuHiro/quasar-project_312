95421e49a162d1658906670d1b7ebbd932ed7ece
TrofuHiro <bajirawat5@gmail.com>
2023-10-13 18:59:04 +0700

    ้ีฟหก

--- test/jest/__tests__/FunctionPage.spec.js
+++ test/jest/__tests__/FunctionPage.spec.js
@@ -7,7 +7,7 @@ describe('IndexPage', () => {
     let header = wrapper.find('.htmlClass h1')
     expect(header.exists()).toBe(true)
     expect(header.text())
-      .toBe('aphiched tepnanta')
+      .toBe('Jirawat kongyoo')
   })
 })
 
