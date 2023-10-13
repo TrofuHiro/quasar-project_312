3688a3457f264b7f4e51025b190bab0b655c6fc3
TrofuHiro <bajirawat5@gmail.com>
2023-10-13 18:28:54 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+
+  <InputPage></InputPage>
+  <FunctionPage></FunctionPage>
+</template>
+
+
+<script>
+import InputPage from "../pages/InputPage.vue"
+import FunctionPage from "../pages/FunctionPage.vue"
+export default {
+  name: 'HtmlComponent',
+  components: {
+    InputPage,
+    FunctionPage
+   },
+  data() {
+    return {
+      title: 'Jirawat kongyoo',
+    }
+  },
+}
+</script>
+
+
+<style scoped></style>
